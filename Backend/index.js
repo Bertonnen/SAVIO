6b46fb3f1f95c9c60f03d61466125acae47f5692
Bertonnen <bertonnenphoto@gmail.com>
2025-05-02 13:06:28 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API de SAVIO funcionando correctamente 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
