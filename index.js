941ca9ee940b401ea27af3501408c07e34246e74
Bertonnen <bertonnenphoto@gmail.com>
2025-06-05 21:45:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
